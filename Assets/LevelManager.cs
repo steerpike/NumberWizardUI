c579d956a80bebda9fcb524069fd533522b96292
Sam Dwyer <Steerpike@Sams-MacBook-Pro.local>
2017-12-06 06:18:49 +1100

    initial commit

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+	public void LoadLevel(string name) {
+		Debug.Log ("Level load requested for "+name);
+	}	
+	public void QuitRequest() {
+		Debug.Log ("Quit game requested");
+	}
+}
